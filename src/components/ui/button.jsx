c0314487c2065eb64994318ffb2b86cd0896d9c8
singhbte <freakout156@gmail.com>
2025-07-12 15:16:59 +0530

    Update button.jsx

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,1 +1,12 @@
 
+import React from 'react';
+
+const Button = ({ text, onClick, className = '' }) => {
+  return (
+    <button onClick={onClick} className={`px-4 py-2 bg-pink-500 text-white rounded ${className}`}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
